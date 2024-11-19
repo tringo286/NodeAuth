91b6d94067d8171631ac109456fecb6175d4e704
Tri Ngo <tringo831@gmail.com>
2024-11-19 10:28:12 -0800

    Set up basic routes and controllers for post and get login and get sign up

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -12,4 +12,23 @@ const signup_post = async (req, res) => {
     }
 };
 
-module.exports = { signup_post };
+const login_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        res.status(200).json({ success: true, user: email});
+    } catch (err) {
+        console.log(err.message);
+        res.status(400).json({ success: false, errors: err});
+    }
+};
+
+const signup_get = (req, res) => {
+    res.send('Sign up form');
+};
+
+const login_get = (req, res) => {
+    res.send('Log in form');
+}
+
+module.exports = { signup_post, login_post, signup_get, login_get };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup_post } = require('../controllers/authControllers');
+const { signup_post, login_post, signup_get, login_get } = require('../controllers/authControllers');
 
 router.post('/signup', signup_post);
+router.post('/login', login_post);
+router.get('/signup', signup_get);
+router.get('/login', login_get);
 
 module.exports = router;
